aadd6a095e9a16ca327e116ede29266c1026cefc
Dillon Drenzek <dillon@bittrex.com>
2018-11-29 20:20:28 -0800

    spectacle topics, site description, security definitions, request examples all added to build json step

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
 var jsonTransform = require('gulp-json-transform');
+var buildV3 = require('./src/_data/build-v3');
 
 gulp.task('build-v3-spec', function() {
-    return gulp.src('./_data/src/api-spec-v3.json')
-        .pipe(jsonTransform(function(data, file) {
-            return data;
-        }))
+    return gulp.src('./src/_data/api-spec-v3.json')
+        .pipe(jsonTransform(buildV3))
         .pipe(gulp.dest('./_data'));
 });
