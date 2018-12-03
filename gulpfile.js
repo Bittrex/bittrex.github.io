87d6b4a016b2f9b106c201f67c363b49ccbd5962
Dillon Drenzek <dillon@bittrex.com>
2018-12-03 12:17:50 -0800

    reorganize v3 spec build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var jsonTransform = require('gulp-json-transform');
-var buildV3 = require('./src/_data/build-v3');
+
+var buildV3Spec = require('./src/v3-spec');
 
 gulp.task('build-v3-spec', function() {
     return gulp.src('./src/_data/api-spec-v3.json')
-        .pipe(jsonTransform(buildV3))
+        .pipe(jsonTransform(buildV3Spec))
         .pipe(gulp.dest('./_data'));
 });
