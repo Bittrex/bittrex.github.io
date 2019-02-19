22cc7447ff1c164efe2cae5a321c7abd8b7ad341
Dillon Drenzek <dillon@bittrex.com>
2019-02-19 15:51:48 -0800

    rename src/v3-spec to src/build-v3-spec

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var jsonTransform = require('gulp-json-transform');
 
-var buildV3Spec = require('./src/v3-spec');
+var buildV3Spec = require('./src/build-v3-spec');
 
 gulp.task('build-v3-spec', function() {
     return gulp.src('./src/_data/api-spec-v3.json')
